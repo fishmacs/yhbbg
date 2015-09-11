f7f25c8ae175ba83769c6a694ac13d89a351f45b
zw <wzhao1984@gmail.com>
2015-09-11 17:17:09 +0800

    fix a bug: can not get testing because wrong url regex produces wrong course id

--- bookbag/testing/urls.py
+++ bookbag/testing/urls.py
@@ -5,7 +5,7 @@ import views
 
 urlpatterns = patterns(
     '',
-    url(r'^get/(\w)+/$', views.get_test),
+    url(r'^get/(\w+)/$', views.get_test),
     url(r'^result/put/$', views.put_result),
     url(r'^result/list/(\w+)/(\w+)/$', views.result_list),
 )
